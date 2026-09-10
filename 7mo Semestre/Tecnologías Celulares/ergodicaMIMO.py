cec2766ea68a692b98199b9b962ef26e1e466755
Yovick RZ <66042604+AyrXZ47@users.noreply.github.com>
2026-09-10 14:03:44 -0600

    fix: speed up ergodic mimo integrals

--- 7mo Semestre/Tecnologías Celulares/ergodicaMIMO.py
+++ 7mo Semestre/Tecnologías Celulares/ergodicaMIMO.py
@@ -15,15 +15,16 @@ for i in range(L):
     # Nt = Nr = N = 2:
     C2[i] = dblquad(lambda y, x: (np.log2(1 + SNR[i] * x / 2) + np.log2(1 + SNR[i] * y / 2)) / 2
                     * np.exp(-x - y) * (x - y) ** 2,
-                    0, 1000, 0, 1000)[0]
+                    0, 1000, 0, 1000, epsabs=1e-5, epsrel=1e-3)[0]
     # Nt = Nr = N = 3:
     C3[i] = tplquad(lambda z, y, x: (np.log2(1 + SNR[i] * x / 3) + np.log2(1 + SNR[i] * y / 3) + np.log2(1 + SNR[i] * z / 3)) / 24
                     * np.exp(-x - y - z) * ((x - y) * (x - z) * (y - z)) ** 2,
-                    0, 10, 0, 10, 0, 10)[0]
+                    0, 10, 0, 10, 0, 10, epsabs=1e-5, epsrel=1e-3)[0]
 
 plt.plot(SNR_dB, C1, "-*", SNR_dB, C2, "-o", SNR_dB, C3, "-s")
 plt.axis([0, 20, 0, 25])
 plt.legend(["N_T=1,N_R=1", "N_T=2,N_R=2", "N_T=3,N_R=3"])
 plt.xlabel("SNR Promedio (dB)", fontsize=10)
 plt.ylabel("Capacidad (bps/Hz)", fontsize=10)
+plt.savefig("ergodicaMIMO.png", dpi=150, bbox_inches="tight")
 plt.show()
